506606e5fb8891e74b6d4374562e94561afbdcf2
skyqii <140032908+skyqii@users.noreply.github.com>
2024-08-25 04:59:51 +0800

    added comments

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from pycaret.anomaly import *
 
 app = Flask(__name__)
 
+# load the model
 model = load_model('anomaly_detection')
 
 @app.route('/')
@@ -13,43 +14,45 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get data from the request
+    # get data from the request
     data1 = request.form["div_name"]
     data2 = request.form["merchant"]
     data3 = request.form["cat_desc"]
     data4 = request.form["trans_dt"]
     data5 = request.form["amt"]
 
+    # convert amount to float
     try:
         data5 = float(data5)
     except ValueError:
         return "Invalid input for amount. Please enter a valid number.", 400
     
-    # Log the data for debugging
+    # log the data for debugging
     print(f"Data received: {data1}, {data2}, {data3}, {data4}, {data5}")
 
-    # Convert the input data into a DataFrame with proper column names
+    # convert the input data into a df with proper column names
     input_data = pd.DataFrame([[data1, data2, data3, data4, data5]],
                               columns=['DIV_NAME', 'MERCHANT', 'CAT_DESC', 'TRANS_DT', 'AMT'])
 
-    # Log the DataFrame for debugging
+    # log df for debugging
     print("Input DataFrame:\n", input_data)
 
-    # Make predictions using the model
+    # make predictions using the model
     prediction_df = predict_model(model,input_data)
 
-    # Log the prediction result for debugging
+    # log the prediction result for debugging
     print("Prediction result:\n", prediction_df)
 
+    # assign variable to the Anomaly result
     prediction = prediction_df['Anomaly'].iloc[0]
 
-    # Check the prediction and return appropriate message
+    # check the prediction and return appropriate message
     if prediction == 0:
         prediction_text = "This record is NOT an anomaly."
     else:
         prediction_text = "This record is an anomaly."
 
-    # Return the prediction to the template
+    # return the prediction to the template
     return render_template('index.html', prediction_text=prediction_text)
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -1,25 +1,30 @@
+# python file for building the model as the pkl file is too large to push into GitHub
+# run this file to the get knn model pickle file
+
 import pandas as pd 
  
 import pycaret 
 pycaret.__version__ 
- 
+
+# reading data in 
 data = pd.read_csv('03_transaction_records.csv') 
  
+# performing data cleaning that cannot be done in the set up
 data.drop_duplicates(inplace=True) 
- 
 data['DIV_NAME'] = data['DIV_NAME'].apply(lambda x: x.upper()) 
 data['CAT_DESC'] = data['CAT_DESC'].apply(lambda x: x.upper()) 
 data['AMT'] = data['AMT'].abs() 
 data['MERCHANT'] = data['MERCHANT'].apply(lambda x: x.upper()) 
  
- 
+# initialize the set up 
 from pycaret.anomaly import * 
 s = setup(data,  
           ignore_features = ['DEPT_NAME', 'FISCAL_YR', 'FISCAL_MTH'], 
           session_id = 123) 
  
- 
+# build the model 
 knn=create_model('knn') 
 # plot_model(knn) 
 
+# save to pkl file
 save_model(knn, 'anomaly_detection')
